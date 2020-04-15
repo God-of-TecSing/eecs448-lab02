d611eb185ba42d8e70b8b50655372a293ae174af
Joshua Bull <joshuapbull@ku.edu>
2020-04-15 13:05:41 -0500

    added null case for remove Back method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -123,9 +123,7 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO
-		Fix this method
-	*/
+	if(m_size == 0)	{	}
 
 	return(isRemoved);
 }
