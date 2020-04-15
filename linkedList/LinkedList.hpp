2670da9ee4ac44167c87f9608c65c2ecbf980568
Joshua Bull <joshuapbull@ku.edu>
2020-04-15 13:02:00 -0500

    added null case for search method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,12 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	if(m_size == 0)
+	{
+
+		return isFound;
+
+	}
 
 	return(isFound);
 }
